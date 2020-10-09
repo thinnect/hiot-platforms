0a6fd581095b961777a74da27596986bc66b7468
Konstantin Bilozor <konstantin@thinnect.com>
2020-10-09 14:38:23 +0300

    Initial WFS201 platform commit.

---
+++ src/boards/wfs201/platform.c
@@ -0,0 +1,68 @@
+/*
+ * H-IoT WFS-201 module.
+ *
+ * Copyright Thinnect Inc. 2020
+ * @license MIT
+ */
+#include "platform.h"
+#include <stdio.h>
+#include <stdint.h>
+#include "em_chip.h"
+#include "em_rmu.h"
+#include "em_emu.h"
+#include "em_cmu.h"
+#include "em_gpio.h"
+#include "em_msc.h"
+
+uint32_t PLATFORM_Init ()
+{
+    volatile uint32_t i;
+    uint32_t resetCause;
+
+    CMU_HFXOInit_TypeDef hfxoInit = CMU_HFXOINIT_DEFAULT;
+    CMU_LFXOInit_TypeDef lfxoInit = CMU_LFXOINIT_DEFAULT;
+
+    // Set the vector table to the start location
+    SCB->VTOR = (uint32_t)VTOR_START_LOCATION;
+
+    resetCause = RMU_ResetCauseGet();
+    RMU_ResetCauseClear();
+
+    for(i = 0; i < 3000000; i++)__asm__("nop");
+
+    if(resetCause & EMU_RSTCAUSE_EM4)
+    {
+        EMU->CMD = EMU_CMD_EM4UNLATCH;
+    }
+
+    CHIP_Init();
+
+    CMU_OscillatorEnable(cmuOsc_FSRCO, 1, 1);
+    CMU_ClockSelectSet(cmuClock_SYSCLK, cmuSelect_FSRCO);
+
+    // Init LFXO
+    CMU_LFXOInit(&lfxoInit);
+
+    // Init HFXO
+    CMU_HFXOInit(&hfxoInit);
+
+    // Set HFXO as source for SYSCLK
+    CMU_ClockSelectSet(cmuClock_SYSCLK, cmuSelect_HFXO);
+
+    // Set HFXO as source for timers
+    CMU_ClockSelectSet(cmuClock_EM01GRPACLK, cmuSelect_HFXO);
+
+    // Disable FSRCO
+    CMU_OscillatorEnable(cmuOsc_FSRCO, 0, 0);
+
+    SystemCoreClockUpdate();
+
+    MSC_Init();
+
+    return resetCause;
+}
+
+void PLATFORM_RadioInit ()
+{
+    // Nothing to do
+}
---
+++ src/boards/wfs201/platform_io.h
@@ -0,0 +1,17 @@
+/**
+ * Platform IO configuration for H-IoT WFS-201 module.
+ *
+ * Copyright Thinnect Inc. 2020
+ * @license MIT
+ */
+
+#ifndef PLATFORM_IO_H_
+#define PLATFORM_IO_H_
+
+#include "em_gpio.h"
+
+#define PLATFORM_LED_COUNT 0
+
+#define PLATFORM_PIN_COUNT 0
+
+#endif//PLATFORM_IO_H_
